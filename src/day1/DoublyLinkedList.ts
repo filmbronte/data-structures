3a9ade4a134e40d9abb3aed5c213a9e9e6ef27e4
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-09-30 02:13:11 +0300

    linked list: prepend, insertAt and get

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -1,27 +1,81 @@
+type Node<T> = {
+    value: T,
+    prev?: Node<T>,
+    next?: Node<T>,
+}
+
 export default class DoublyLinkedList<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
     prepend(item: T): void {
+        const node = { value: item } as Node<T>;
 
-}
+        this.length++;
+
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+
+        node.next = this.head;
+        this.head.prev = node;
+        this.head = node;
+
+    }
     insertAt(item: T, idx: number): void {
 
-}
-    append(item: T): void {
+        if (idx > this.length) {
+            throw new Error('error');
+        } 
 
-}
+        if (idx === this.length) {
+            this.append(item);
+            return;
+        } else if (idx === 0) {
+            this.prepend(item);
+            return;
+        }
+        this.length++;
+
+        let curr = this.head;
+        for (let i = 0; curr && i < idx; ++i) {
+            curr = curr.next;
+        }
+
+        curr = curr as Node<T>;
+        const node = {value: item} as Node<T>;
+
+        node.next = curr;
+        node.prev = curr.prev;
+        curr.prev = node;
+
+        if (curr.prev) {
+            curr.prev.next = curr;
+        }
+    }
+    append(item: T): void {
+        
+    }
     remove(item: T): T | undefined {
 
-}
+    }
     get(idx: number): T | undefined {
+        let curr = this.head;
+        for (let i = 0; i < idx && curr; ++i) {
+            curr = curr.next;
+        }
 
-}
+        return curr?.value;
+
+    }
     removeAt(idx: number): T | undefined {
 
-}
+    }
 }
